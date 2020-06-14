ff6826169ff8bd3c07b58171048471f8445fe95c
Munaif <pentest443@outlook.com>
2020-06-14 09:15:26 -0700

    Insert Categories

--- admin/categories.php
+++ admin/categories.php
@@ -1,16 +1,184 @@
-<?php include "inc/header.php";  ?>
+<?php $page_title = "Categories"; ?>
+<?php include "inc/header.php"; ?>
+<?php include "inc/functions.php"; ?>
+<?php include "inc/navbar.php"; ?>
 
-<?php include "inc/navbar.php";  ?>
+<?php $cats = "active"; ?>
+
+<?php 
+	$name = "";
+		
+	if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+		if(isset($_POST['addcategory'])){
+
+			$name = filter_input(INPUT_POST,'name',FILTER_SANITIZE_STRING);
+			date_default_timezone_set("Africa/Cairo");
+			$datetime = date('M-d-Y h:m', time());
+			$creater_name = "Ebrahem"; // temporary name until creating admins
+
+			$error_msg = "";
+			if( strlen($name) < 5 || strlen($name) > 50) {
+				$error_msg = "Category Name must be between 5 and 50 character";
+			}
+
+			if(empty($error_msg)) {
+				if (! session_id()){
+					session_start();
+				}
+				if(insert_category($datetime,$name,$creater_name)) {
+					$_SESSION['success'] = "Category has been Added Successfully";
+					redirect("categories.php");					
+				}else {
+					$_SESSION['error'] = "Unable to Insert Category";
+					redirect("categories.php");	
+				}
+			}else {
+				if (! session_id()){
+					session_start();
+				}
+				$_SESSION['error'] = $error_msg;
+				redirect("categories.php");	
+			}	
+
+		} else {
+
+			if(isset($_POST['updatecategory'])) {
+
+				$id = filter_input(INPUT_POST,'id',FILTER_SANITIZE_NUMBER_INT);
+				$name = filter_input(INPUT_POST,'name',FILTER_SANITIZE_STRING);
+
+				$error_msg = "";
+				if( strlen($name) < 5 || strlen($name) > 50) {
+					$error_msg = "Category Name must be between 5 and 50 character";
+				}
+
+				if(empty($error_msg)) {
+					if (! session_id()){
+						session_start();
+					}
+					if(update_category($name, $id)) {
+						$_SESSION['success'] = "Category has been Updated Successfully";
+						redirect("categories.php");					
+					}else {
+						$_SESSION['error'] = "Unable to Update Category";
+						redirect("categories.php");	
+					}
+				}else {
+					if (! session_id()){
+						session_start();
+					}
+					$_SESSION['error'] = $error_msg;
+					redirect("categories.php");	
+				}	
+			}
+		}
+
+	} else {
+		if(isset($_GET['id']) && ! empty($_GET['id'])) {
+
+			$id = filter_input(INPUT_GET,'id',FILTER_SANITIZE_NUMBER_INT);
+
+			$category = get_categories($id);
+			$name = $category['name'];
+		}
+	}
+
+?>
+
 
 <div class="container-fluid">
-  <div class="row">
-    <div class="col-sm-2">
-  	    <?php include "inc/sidebar.php"; ?>
-    </div>
-    <div class="col-sm">
-      This is categories page
-    </div>
-  </div>
+	<div class="row">
+		<?php include "inc/media_sidebar.php"; ?>
+		<div class="col-sm-2">
+			<?php include "inc/sidebar.php"; ?>
+		</div>
+		<div class="col-sm">
+			
+			<div class="categories">
+				<?php 
+					if( ! session_id() ) {
+						session_start();
+					}
+					if( isset($_SESSION['success']) && ! empty($_SESSION['success'])) {
+						echo "<div class='alert alert-success'>";
+						echo $_SESSION['success'];
+						echo "</div>";
+						$_SESSION['success'] = "";
+					}
+					if( isset($_SESSION['error']) && ! empty($_SESSION['error'])) {
+						echo "<div class='alert alert-danger'>";
+						echo $_SESSION['error'];
+						echo "</div>";
+						$_SESSION['error'] = "";
+					}
+				?>
+
+				<h4>Categories</h4>
+				<form action="categories.php" method="POST">
+					<div class="row">
+						<div class="col-sm-10">
+							<div class="form-group">
+								<input type="hidden" name="id" value="<?php echo $id; ?>">
+								<input value="<?php echo $name; ?>" type="text" name="name" class="form-control" placeholder="Category Name" autocomplete="off" required>
+							</div>
+						</div>
+						<div class="col-sm">
+							<?php if(isset($_GET['id'])) {
+								echo '<input value="Update Category" type="submit" name="updatecategory" class="btn btn-primary">';
+							}else { ?>
+								<input value="Add Category" type="submit" name="addcategory" class="btn btn-primary">
+							<?php } ?>
+						</div>
+					</div>
+				</form>
+				<div class="table-responsive">
+					<table class="table table-hover table-striped table-dark">
+					  <thead>
+					    <tr>
+					      <th scope="col">#</th>
+					      <th scope="col">datetime</th>
+					      <th scope="col">Category</th>
+					      <th scope="col">Created_by</th>
+					      <th scope="col">Actions</th>
+					    </tr>
+					  </thead>
+					  <tbody>
+
+					  	<?php
+					  	$number = 0;
+					  	foreach(get_categories() as $category) { $number++; ?>
+
+					    <tr>
+					      <th scope="row"><?php echo $number; ?></th>
+					      <td><?php echo $category['datetime']; ?></td>
+					      <td class="cat_name">
+					      	<?php 
+					      		echo $category['name'];
+					      	?>
+					      	
+					      </td>
+					      <td>
+					      	<?php 
+					      		echo $category['creater_name'];
+					      	?>
+					      </td>
+					      <td class="action-links">
+					      	<a class="btn btn-primary btn-sm" href="categories.php?id=<?php echo $category['id']; ?>">Edit</a>
+					      	<form onsubmit="return confirm('Are You Sure?');" action="deletecategory.php" method="POST">
+					      		<input type="hidden" name="id" value="<?php echo $category['id']; ?>">
+					      		<input class="btn btn-danger btn-sm" type="submit" value="Delete" name="deletecategory">
+					      	</form>
+					      </td>
+					    </tr>
+						<?php } ?>
+					  </tbody>
+					</table>
+				</div>
+			</div>
+
+		</div>
+	</div>
 </div>
 
 <?php include "inc/footer.php"; ?>
--- admin/inc/functions.php
+++ admin/inc/functions.php
@@ -1,7 +1,5 @@
 <?php 
-
-
-/* Category function */
+/* Categoryu functions */
 function get_categories($id = "") {
 	include "connect.php";
 	$sql = "";
@@ -10,7 +8,6 @@ function get_categories($id = "") {
 	}else {
 		$sql = "SELECT * FROM categories WHERE id = ? ";
 	}
-
 	try {
 		if(empty($id)){
 			$result = $con->query($sql);
@@ -27,14 +24,40 @@ function get_categories($id = "") {
 		return array();
 	}
 }
+function insert_category($datetime,$name,$creater_name) {
+	$fields = array($datetime, $name, $creater_name);
+	include "connect.php";
+	$sql = "INSERT INTO categories (datetime, name, creater_name) VALUES (?,?,?)";
+	try{
+		$result = $con->prepare($sql);
+		for($i = 1; $i <= 3; $i++){
+			$result->bindValue($i, $fields[$i - 1], PDO::PARAM_STR);
+		}
+		return $result->execute();
+	}catch(Exception $e) {
+		echo "Error: ". $e->getMessage();
 
+		return false;
+	}
+}
+function update_category($name, $id) {
+	include "connect.php";
+	$sql = "UPDATE categories SET name = ? WHERE id = ?";
+	try {
+		$result = $con->prepare($sql);
+		$result->bindValue(1,$name, PDO::PARAM_STR);
+		$result->bindValue(2,$id, PDO::PARAM_INT);
+		return $result->execute();
+	}catch(Exception $e) {
+		echo "Error: " .$e->getMessage();
+		return false;
+	}
+}
 /* Post Functions */
-
 function insert_post($datetime, $title, $content, $author, $excerpt, $image, $category, $tags) {
 	$fields = array($datetime, $title, $content, $author, $excerpt, $image, $category, $tags);
 	include "connect.php";
 	$sql = "INSERT INTO posts (datetime, title, content, author, excerpt, image, category, tags) VALUES (?,?,?,?,?,?,?,?) ";	
-
 	try{
 			$result = $con->prepare($sql);
 			for($i = 1; $i <= 8; $i++){
@@ -46,7 +69,6 @@ function insert_post($datetime, $title, $content, $author, $excerpt, $image, $ca
 			return false;
 		}
 	}
-
 function get_posts($id = "") {
 	include "connect.php";
 	$sql = "";
@@ -55,14 +77,11 @@ function get_posts($id = "") {
 	}else {
 		$sql = "SELECT * FROM posts WHERE id = ?";
 	}
-	
 	try {
-
 		if(! empty($id)){
 			$result = $con->prepare($sql);
 			$result->bindValue(1, $id, PDO::PARAM_INT);
 			$result->execute();
-
 			return $result->fetch(PDO::FETCH_ASSOC);
 		}else {
 			$result = $con->query($sql);
@@ -74,14 +93,12 @@ function get_posts($id = "") {
 		return array();
 	}
 }
-
 function delete($table, $id) {
 	include "connect.php";
 	$sql = "DELETE FROM $table WHERE id = ? ";
 	try{
 		$result = $con->prepare($sql);
 		$result->bindValue(1, $id, PDO::PARAM_INT);
-
 		return $result->execute();
 	}
 	catch(Exception $e) {
@@ -89,8 +106,6 @@ function delete($table, $id) {
 		return false;
 	}
 }
-
-
 function update_post($title, $content, $excerpt, $image = "", $category, $tags, $id) {
 	$fields = array($title, $content, $excerpt,$category, $tags);
 	include "connect.php";
@@ -101,12 +116,10 @@ function update_post($title, $content, $excerpt, $image = "", $category, $tags, 
 		$sql = "UPDATE posts SET title = ?, content = ?, excerpt = ?, category = ?, tags = ?, image = ? WHERE id = ?";
 	}
 	try {
-
 		$result = $con->prepare($sql);
 		for($i = 1; $i <= 5; $i++){
 			$result->bindValue($i, $fields[$i - 1], PDO::PARAM_STR);
 		}
-
 		if(! empty($image)) {
 			$result->bindValue(6, $image, PDO::PARAM_STR);
 			$result->bindValue(7,$id,PDO::PARAM_INT);
@@ -119,8 +132,6 @@ function update_post($title, $content, $excerpt, $image = "", $category, $tags, 
 		return false;
 	}
 }
-
-
 function redirect($location) {
 	header("location: posts.php");
 	exit;
